a2358f366e7cd154cf6ebac77263b65cbe7c7216
Kaifeng Lu <kaifenglu@gmail.com>
2023-03-09 11:23:21 -0500

    use lower case and replace adt with time

--- R/data.R
+++ R/data.R
@@ -1,14 +1,15 @@
 #' Observed enrollment and event data
 #'
-#' Simulated enrollment and event data in examples.
+#' Simulated enrollment and event data used in the examples.
 #'
 #' @format ## `observedData`
 #' A data frame with 300 rows and 5 columns:
 #' \describe{
-#'   \item{RANDDT}{Randomization date}
-#'   \item{CUTOFFDT}{Cutoff date of the data set}
-#'   \item{ADT}{Date of event or censoring}
-#'   \item{EVENT}{Event indicator}
-#'   \item{DROPOUT}{Dropout indicator}
+#'   \item{\code{randdt}}{Randomization date}
+#'   \item{\code{cutoffdt}}{Cutoff date of the data set}
+#'   \item{\code{time}}{Day of event or censoring since randomization}
+#'   \item{\code{event}}{Event indicator: 1 for event, 0 for non-event}
+#'   \item{\code{dropout}}{Dropout indicator: 1 for dropout, 0 for non-dropout}
 #' }
+#' For ongoing subjects, both \code{event} and \code{dropout} are equal to 0.
 "observedData"
