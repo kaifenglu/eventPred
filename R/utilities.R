be22aaf26c97108f6e9354118e2b68884ec6fca5
Kaifeng Lu <kaifenglu@gmail.com>
2025-02-27 11:23:46 -0500

    - update x to denote the covariates vector or design matrix including the intercept, and q to denote the length of the covariate vector or the number of columns of the covariates matrix excluding the intercept
    - remove log-likelihood and add profile log-likelihood for piecewise exponential regression
    
    - update the time intervals to be left-closed and right-open for piecewise exponential models
    
    - obtain the maximum likelihood estimates of piecewise exponential regression model parameters by maximizing the profile log-likelihood instead of maximizing the full log-likelihood

--- R/utilities.R
+++ R/utilities.R
@@ -10,8 +10,9 @@
 #'   for the piecewise exponential survival distribution.
 #'   Must start with 0, e.g., c(0, 60) breaks the time axis into 2 event
 #'   intervals: [0, 60) and [60, Inf). By default, it is set to 0.
-#' @param q The number of elements in the vector of covariates.
-#' @param x The vector of covariates (excluding the intercept).
+#' @param q The number of elements in the vector of covariates
+#'   (excluding the intercept).
+#' @param x The vector of covariates (including the intercept).
 #' @param lower.tail logical; if TRUE (default), probabilities are
 #'   the distribution function, otherwise, the survival function.
 #' @param log.p logical; if TRUE, probabilities p are given as log(p).
@@ -39,7 +40,7 @@ ppwexp <- function(t, theta, J, tcut, q = 0, x = 0, lower.tail = TRUE,
   cumhaz = psum[j1] + lambda[j1]*(t - tcut[j1])
 
   if (q > 0) {
-    xbeta = sum(x * theta[(J+1):(J+q)])
+    xbeta = sum(x[-1] * theta[(J+1):(J+q)])
     cumhaz = cumhaz*exp(xbeta)
   }
 
@@ -63,8 +64,9 @@ ppwexp <- function(t, theta, J, tcut, q = 0, x = 0, lower.tail = TRUE,
 #'   for the baseline piecewise exponential survival distribution.
 #'   Must start with 0, e.g., c(0, 60) breaks the time axis into 2 event
 #'   intervals: [0, 60) and [60, Inf). By default, it is set to 0.
-#' @param q The number of elements in the vector of covariates.
-#' @param x The vector of covariates (excluding the intercept).
+#' @param q The number of elements in the vector of covariates
+#'   (excluding the intercept).
+#' @param x The vector of covariates (including the intercept).
 #' @param lower.tail logical; if TRUE (default), probabilities are
 #'   the distribution function, otherwise, the survival function.
 #' @param log.p logical; if TRUE, probabilities p are given as log(p).
@@ -93,7 +95,7 @@ qpwexp <- function(p, theta, J, tcut, q = 0, x = 0, lower.tail = TRUE,
   cumhaz0 = -log(1-p)
 
   if (q > 0) {
-    xbeta = sum(x * theta[(J+1):(J+q)])
+    xbeta = sum(x[-1] * theta[(J+1):(J+q)])
     cumhaz0 = cumhaz0*exp(-xbeta)
   }
 
@@ -104,12 +106,11 @@ qpwexp <- function(p, theta, J, tcut, q = 0, x = 0, lower.tail = TRUE,
 }
 
 
-#' @title Log likelihood for piecewise exponential regression
-#' @description Obtains the log likelihood value for piecewise
+#' @title Profile log likelihood for piecewise exponential regression
+#' @description Obtains the profile log likelihood value for piecewise
 #' exponential regression.
 #'
-#' @param theta The parameter vector consisting of gamma for log
-#'   piecewise hazards and beta for regression coefficients.
+#' @param beta The regression coefficients with respect the covariates.
 #' @param time The survival time.
 #' @param event The event indicator.
 #' @param J The number of time intervals.
@@ -117,31 +118,30 @@ qpwexp <- function(p, theta, J, tcut, q = 0, x = 0, lower.tail = TRUE,
 #'   for the baseline piecewise exponential survival distribution.
 #'   Must start with 0, e.g., c(0, 60) breaks the time axis into 2 event
 #'   intervals: [0, 60) and [60, Inf). By default, it is set to 0.
-#' @param q The number of columns of the covariates matrix.
-#' @param x The covariates matrix (excluding the intercept).
+#' @param q The number of columns of the covariates matrix
+#'   (excluding the intercept).
+#' @param x The covariates matrix (including the intercept).
 #'
-#' @return The log likelihood value for piecewise exponential regression.
+#' @return The profile log likelihood value for piecewise exponential
+#' regression.
 #'
 #' @keywords internal
 #'
 #' @export
 #'
-llik_pwexp <- function(theta, time, event, J, tcut, q, x) {
+pllik_pwexp <- function(beta, time, event, J, tcut, q, x) {
   if (length(tcut) == J) tcut = c(tcut, Inf)
 
-  d = 0
-  ex = 0
+  xbeta = as.numeric(x[,-1] %*% beta)
+
+  d = rep(0,J)
+  ex0 = rep(0,J)
   for (j in 1:J) {
-    d = d + theta[j] * (time > tcut[j]) * (time <= tcut[j+1])
-    ex = ex + exp(theta[j]) * pmax(0, pmin(time, tcut[j+1]) - tcut[j])
+    d[j] = sum(event * (time >= tcut[j]) * (time < tcut[j+1]))
+    ex0[j] = sum(pmax(0, pmin(time, tcut[j+1]) - tcut[j])*exp(xbeta))
   }
 
-  if (q == 0) {
-    sum(event*d - ex)
-  } else {
-    xbeta = as.numeric(x %*% theta[(J+1):(J+q)])
-    sum(event * (d + xbeta) - ex * exp(xbeta))
-  }
+  sum(event*xbeta) - sum(d*log(ex0))
 }
 
 
@@ -156,8 +156,9 @@ llik_pwexp <- function(theta, time, event, J, tcut, q, x) {
 #'   for the baseline piecewise exponential survival distribution.
 #'   Must start with 0, e.g., c(0, 60) breaks the time axis into 2 event
 #'   intervals: [0, 60) and [60, Inf). By default, it is set to 0.
-#' @param q The number of columns of the covariates matrix.
-#' @param x The covariates matrix (excluding the intercept).
+#' @param q The number of columns of the covariates matrix
+#'   (exluding the intercept).
+#' @param x The covariates matrix (including the intercept).
 #'
 #' @return The maximum likelihood estimates and the associated
 #' covariance matrix, AIC and BIC.
@@ -172,15 +173,15 @@ pwexpreg <- function(time, event, J, tcut, q, x) {
     variable_name = "event"
   }
 
-  if (length(tcut) == J) {
-    ucut = c(tcut, Inf)
-  } else {
-    ucut = tcut
-  }
+  if (length(tcut) == J) tcut = c(tcut, Inf)
 
-  d = rep(0, J)  # number of events in each interval
+  n = length(time)
+
+  d = rep(0,J)  # number of events in each interval
+  exmat = matrix(0,n,J)
   for (j in 1:J) {
-    d[j] = sum((time > ucut[j]) * (time <= ucut[j+1]) * event)
+    d[j] = sum(event * (time >= tcut[j]) * (time < tcut[j+1]))
+    exmat[,j] = pmax(0, pmin(time, tcut[j+1]) - tcut[j])
   }
 
   if (any(d == 0)) {
@@ -189,34 +190,96 @@ pwexpreg <- function(time, event, J, tcut, q, x) {
                "to fit a piecewise exponential model."))
   }
 
-  theta0 = rep(0, J+q)
+  if (q == 0) { # piecewise exponential with no covariates
+    ex = colSums(exmat)  # exposure in each interval
 
-  if (J+q > 1) {
-    theta0 = rep(0, J+q)
-    opt1 <- optim(theta0, llik_pwexp, gr = NULL,
-                  time, event, J, ucut, q, x,
-                  control = c(fnscale = -1), hessian = TRUE)
-  } else { # exponential distribution with no covariates
-    theta0 = log(sum(event)/sum(time))
-    opt1 <- optim(theta0, llik_pwexp, gr = NULL,
-                  time, event, J, ucut, q, x,
-                  method = "Brent",
-                  lower = theta0 - 2, upper = theta0 + 2,
-                  control = c(fnscale = -1), hessian = TRUE)
+    theta = log(d/ex)
+    if (J > 1) {
+      vtheta = diag(1/d)
+    } else {
+      vtheta = 1/d*diag(1)
+    }
+
+    llik = sum(theta*d - exp(theta)*ex)
+
+    fit <- list(model = "Piecewise exponential",
+                theta = theta,
+                vtheta = vtheta,
+                aic = -2*llik + 2*J,
+                bic = -2*llik + J*log(n))
+  } else { # piecewise exponential with covariates
+    beta0 = rep(0,q)
+    if (q > 1) {
+      opt1 <- optim(beta0, pllik_pwexp, gr = NULL,
+                    time, event, J, tcut, q, x,
+                    control = c(fnscale = -1), hessian = TRUE)
+    } else {
+      sdx = sd(x[,-1])
+      opt1 <- optim(beta0, pllik_pwexp, gr = NULL,
+                    time, event, J, tcut, q, x,
+                    method = "Brent",
+                    lower = beta0 - 6*sdx, upper = beta0 + 6*sdx,
+                    control = c(fnscale = -1), hessian = TRUE)
+    }
+
+    beta = opt1$par
+    vbeta = solve(-opt1$hessian)
+
+    xbeta = as.numeric(x[,-1] %*% beta)
+    exbeta = exp(xbeta)
+
+    ex0 = rep(0,J)
+    for (j in 1:J) {
+      ex0[j] = sum(exmat[,j]*exbeta)
+    }
+
+    ex1 = matrix(0,J,q)
+    for (j in 1:J) {
+      for (k in 1:q) {
+        ex1[j,k] = sum(exmat[,j]*x[,k+1]*exbeta)
+      }
+    }
+
+    rx1 = matrix(0,J,q)
+    for (j in 1:J) {
+      rx1[j,] = ex1[j,]/ex0[j]
+    }
+
+    vtheta = matrix(0, J+q, J+q)
+    for (j in 1:J) {
+      for (k in 1:J) {
+        vtheta[j,k] = 1/d[j]*(j==k) + rx1[j,] %*% vbeta %*% rx1[k,]
+      }
+    }
+
+    for (j in 1:J) {
+      for (k in 1:q) {
+        vtheta[j,J+k] = -rx1[j,] %*% vbeta[,k]
+        vtheta[J+k,j] = vtheta[j,J+k]
+      }
+    }
+
+    for (k1 in 1:q) {
+      for (k2 in 1:q) {
+        vtheta[J+k1,J+k2] = vbeta[k1,k2]
+      }
+    }
+
+    alpha = log(d/ex0)
+    theta = c(alpha, beta)
+    llik = sum(alpha*d) + sum(event*xbeta) - sum(exp(alpha)*ex0)
+
+    fit <- list(model = "Piecewise exponential",
+                theta = theta,
+                vtheta = vtheta,
+                aic = -2*llik + 2*(J+q),
+                bic = -2*llik + (J+q)*log(n))
   }
 
-  n = length(time)
-
-  fit <- list(model = "Piecewise exponential",
-              theta = opt1$par,
-              vtheta = solve(-opt1$hessian),
-              aic = -2*opt1$value + 2*(J+q),
-              bic = -2*opt1$value + (J+q)*log(n))
-
   if (variable_name == "event") {
-    fit$piecewiseSurvivalTime = tcut
+    fit$piecewiseSurvivalTime = tcut[1:J]
   } else {
-    fit$piecewiseDropoutTime = tcut
+    fit$piecewiseDropoutTime = tcut[1:J]
   }
 
   fit
@@ -232,8 +295,9 @@ pwexpreg <- function(time, event, J, tcut, q, x) {
 #'   time (AFT) regression coefficients and the logrithm of the AFT
 #'   regression scale parameter for the Weibull and log-normal distributions.
 #' @param w1 The weight for the Weibull component distribution.
-#' @param q The number of elements in the vector of covariates.
-#' @param x The vector of covariates (excluding the intercept).
+#' @param q The number of elements in the vector of covariates
+#'   (excluding the intercept).
+#' @param x The vector of covariates (including the intercept).
 #' @param lower.tail logical; if TRUE (default), probabilities are
 #'   the distribution function, otherwise, the survival function.
 #' @param log.p logical; if TRUE, probabilities p are given as log(p).
@@ -246,8 +310,8 @@ pwexpreg <- function(time, event, J, tcut, q, x) {
 #'
 pmodavg <- function(t, theta, w1, q, x, lower.tail = TRUE, log.p = FALSE) {
   shape = exp(-theta[q+2])
-  scale = exp(sum(c(1,x) * theta[1:(q+1)]))
-  meanlog = sum(c(1,x) * theta[(q+3):(2*q+3)])
+  scale = exp(sum(x * theta[1:(q+1)]))
+  meanlog = sum(x * theta[(q+3):(2*q+3)])
   sdlog = exp(theta[2*q+4])
 
   p1 = pweibull(t, shape, scale)
